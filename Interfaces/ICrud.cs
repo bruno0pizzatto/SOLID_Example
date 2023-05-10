57194418a334621c62a0d6e7b0b1bccad75d6665
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-05-10 13:19:38 -0300

    Add project files.

---
+++ Interfaces/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using SOLID_Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Example.Interfaces
+{
+    public interface ICrud<Element> where Element : BaseEntity
+    {
+        public void Add(Element element);
+        public void Delete(Element element);
+        public void Update(Element element);
+        public List<Element> GetAll();  
+    }
+}
---
+++ Interfaces/IDatabase.cs
@@ -0,0 +1,15 @@
+﻿using SOLID_Example.Models;
+
+namespace SOLID_Example.Interfaces
+{
+    public interface IDatabase<Element> where Element : BaseEntity
+    {
+        IEnumerable<Element> GetAll();
+        bool Add(Element element);
+        bool Remove(Element element);
+        bool Update(Element element);
+        void CleanAll();
+        void Dispose();
+        IEnumerable<Element> GetElements();
+    }
+}
